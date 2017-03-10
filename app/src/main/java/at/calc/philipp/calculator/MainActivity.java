c7adb1fc7ed5a47b708b70507467fb3387bf31b7
Philipp Starzacher <p.starzacher@gmail.com>
2017-03-10 13:37:27 +0100

    Added some function

--- app/src/main/java/at/calc/philipp/calculator/MainActivity.java
+++ app/src/main/java/at/calc/philipp/calculator/MainActivity.java
@@ -2,12 +2,58 @@ package at.calc.philipp.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputDividend;
+    EditText inputDivisor;
+    TextView outputResult;
+    TextView labelResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        labelResult = (TextView) findViewById(R.id.txtView_resultLabel);
+        outputResult = (TextView) findViewById(R.id.txtView_result);
+        inputDividend = (EditText) findViewById(R.id.input_dividend);
+        inputDivisor = (EditText) findViewById(R.id.input_divisor);
+    }
+
+    public void setResultVisibility(boolean visibel) {
+        if (visibel) {
+            outputResult.setVisibility(View.VISIBLE);
+            labelResult.setVisibility(View.VISIBLE);
+        } else {
+            outputResult.setVisibility(View.INVISIBLE);
+            labelResult.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void divide(View view){
+        String dividend = inputDividend.getText().toString();
+        String divisor = inputDivisor.getText().toString();
+
+        if(dividend == "" || divisor == ""){
+
+        }
+
+
+
+        double result = dividend / divisor;
+
+        outputResult.setText(String.valueOf(result));
+
+        setResultVisibility(true);
+    }
+
+    public void sendErrorMessage(String msg){
+
     }
 }
