8fc8e7b6e748d0775c3eb40f30c4d66d8b1d6c29
Philipp Starzacher <p.starzacher@gmail.com>
2017-03-13 17:18:09 +0100

    Virtual Keyboard closes after Button is touched

--- app/src/main/java/at/calc/philipp/calculator/MainActivity.java
+++ app/src/main/java/at/calc/philipp/calculator/MainActivity.java
@@ -1,9 +1,11 @@
 package at.calc.philipp.calculator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
             outputResult.setText(result);
         }
 
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
 
     }
 }
